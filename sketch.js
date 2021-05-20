a34ee06edbac4a3b8634d01b22af244015a695a2
himanshurai2999 <77000527+himanshurai2999@users.noreply.github.com>
2021-05-20 12:04:38 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var contestantCount;
+var allContestants;
+var answer;
+var database;
+
+var question, contestant, quiz;
+
+
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+  quiz = new Quiz();
+  quiz.getState();
+  quiz.start();
+}
+
+
+function draw(){
+  background("pink");
+  if(contestantCount === 2){
+    quiz.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    quiz.play();
+  }
+}
+
+for(var plr in allcontestants){
+  var correctAns ="2";
+  if(correctAns === allContestant[plr].answer)
+  fill("Green")
+  else
+  fill("red");
+}
+
+
+
